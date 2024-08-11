3a6c13399f0abb5dcbea73b80258febd8df1558b
suryadigit <211111397@students.mikroskil.ac.id>
2024-08-11 23:38:22 +0700

    chore: Refactor chat service to use final fields and improve API request handling

--- suryaichat/lib/services/chat_service.dart
+++ suryaichat/lib/services/chat_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
@@ -7,88 +9,70 @@ class ChatService {
   final stt.SpeechToText _speech = stt.SpeechToText();
   late String apiKey;
   late String apiUrl;
-  List<Map<String, String>> conversationHistory = [];
+  List<Map<String, String>> _conversationHistory = [];
 
   ChatService() {
-    _initialize();
-  }
-
-  Future<void> _initialize() async {
-    await loadApiKey();
-    await initializeSpeech();
+    loadApiKey();
   }
 
   Future<void> loadApiKey() async {
-    final loaded = dotenv.env['API_KEY'];
-    final url = dotenv.env['API_URL'];
-
-    if (loaded == null || url == null) {
-      throw Exception('API_KEY or API_URL is not set in .env file');
-    }
-
-    apiKey = loaded;
-    apiUrl = url;
+    apiKey = dotenv.env['API_KEY']!;
+    apiUrl = dotenv.env['API_URL']!;
   }
 
   Future<String> sendMessage(String userInput) async {
-    conversationHistory.add({"role": "user", "content": userInput});
+    _conversationHistory.add({"role": "user", "content": userInput});
 
     try {
-      final response = await http.post( 
+      final response = await http.post(
         Uri.parse(apiUrl),
         headers: <String, String>{
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $apiKey',
+          'Content-Type': 'application/json;charset=UTF-8',
+          'Charset': 'utf-8',
+          'Authorization': 'Bearer $apiKey'
         },
         body: jsonEncode(
           <String, dynamic>{
-            "model":
-                "gpt-3.5-turbo",
+            "model": "gpt-3.5-turbo",
             "messages": [
               {
                 "role": "system",
-                "content": '''
-                  Anda adalah asisten AI yang bernama suryaichat yang cerdas, responsif, 
-                  dan asik diajak ngobrol.
-                  Jawablah pertanyaan dengan gaya santai dan humor ala warga +62 yang random 
-                  suka ngomong anjay, anjir,dan serius, dan selalu memberikan informasi yang akurat dan
-                  bermanfaat. bisa juga memberikan link website yang dicari oleh user.
-                  Jadilah teman curhat yang selalu punya jawaban lengkap dan memadai.
-                '''
+                "content":
+                    '''Anda adalah asisten AI yang bernama suryaichat yang cerdas, responsif, 
+                    dan asik diajak ngobrol.
+                    Jawablah pertanyaan dengan gaya santai dan humor ala warga +62 yang random 
+                    suka ngomong anjay, anjir,dan serius, dan selalu memberikan informasi yang akurat dan
+                    bermanfaat. bisa juga memberikan link website yang dicari oleh user.
+                    Jadilah teman curhat yang selalu punya jawaban lengkap dan memadai.'''
               },
-              ...conversationHistory,
+              ..._conversationHistory,
             ],
-            'max_tokens': 300, 
+            'max_tokens': 1000,
           },
         ),
       );
 
       if (response.statusCode == 200) {
         final responseData = jsonDecode(response.body);
+        String aiResponse = responseData['choices'][0]['message']['content'];
 
-        if (responseData['choices'] != null &&
-            responseData['choices'].isNotEmpty) {
-          String aiResponse = responseData['choices'][0]['message']['content'];
-          String filteredResponse = _filterResponse(aiResponse);
-
-          conversationHistory
-              .add({"role": "assistant", "content": aiResponse});
-          return filteredResponse;
-        } else {
-          throw Exception('Invalid response format.');
+        String filteredResponse = '';
+        for (int i = 0; i < aiResponse.length; i++) {
+          String char = aiResponse[i];
+          if (RegExp(r'[a-zA-Z0-9\s.,!?`~@#$%^&*()_+\-=\[\]{};:"\\|,.<>/?]')
+              .hasMatch(char)) {
+            filteredResponse += char;
+          }
         }
+
+        _conversationHistory.add({"role": "assistant", "content": aiResponse});
+        return filteredResponse;
       } else {
-        throw Exception(
-            'Failed to get response from server. Status code: ${response.statusCode}');
+        throw Exception('Failed to get response from server.');
       }
     } catch (e) {
       throw Exception('Error occurred while sending message: $e');
     }
-  }
-
-  String _filterResponse(String response) {
-    return response.replaceAll(
-        RegExp(r'[^\w\s.,!?`~@#$%^&*()_+\-=\[\]{};:"\\|,.<>/?]'), '');
   }
 
   Future<void> initializeSpeech() async {
